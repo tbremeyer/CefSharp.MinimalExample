d0623e48052460f585056309f64e8be37ab721fd
Bremeyer, Torsten <Torsten.Bremeyer@unisys.com>
2020-06-16 08:46:37 +0200

    fixed reference errors

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace CefSharp.MinimalExample.Wpf
@@ -22,9 +23,46 @@ namespace CefSharp.MinimalExample.Wpf
         private async Task CallWebSite()
         {
             if (!_firstTime) return;
-            //Browser.ShowDevTools();
+            Browser.ShowDevTools();
             var result = await Browser.EvaluateScriptAsync($"addWMSLayer('http://wms.zh.ch/Raster1000CWMS', 'Landeskarte 1:500000 / web.wms.zh.ch', 'http://web.wms.zh.ch', 'lk500', 'image/png', 'mapserver', '369997', '320000', '280000', '220000')");
             _firstTime = false;
+            initializeMap();
         }
-    }
+        
+        private async Task initializeMap()
+        {
+            await this.addMarker();
+            await this.zoomMap();
+        }     
+        
+        private async Task addMarker()
+        {
+            try
+            {
+                var response = await Browser.EvaluateScriptAsync("addMarker()", new TimeSpan(0, 0, 1));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }    
+   
+        private async Task zoomMap()
+        {
+            try
+            {
+                var east = 2679144.1499999999m;
+                var north = 1247313.99m;
+                int defaultZoom = 12;
+
+                //var script = string.Format("zoomMap('{0}', '{1}')", east, north);
+                var script = $"setDefaultParameter('{east}', '{north}', '{defaultZoom}')";
+                Browser.ExecuteScriptAsync(script);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }        
+   }
 }
